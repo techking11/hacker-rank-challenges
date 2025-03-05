fef96ead1c98db65986994e3748d391d56f62180
techking11 <nma1132000@gmail.com>
2025-03-05 14:49:03 +0700

    add plusMin from hackerank
    output with 6 decimals in positive, negative, zero of integer array

---
+++ one-week-challenge/01-first-day/plusMinus.js
@@ -0,0 +1,59 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+  let n = arr.length;
+  let positiveCount = 0;
+  let negativeCount = 0;
+  let zeroCount = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (arr[i] > 0) {
+      positiveCount++;
+    } else if (arr[i] < 0) {
+      negativeCount++;
+    } else {
+      zeroCount++;
+    }
+  }
+
+  process.stdout.write((positiveCount / n).toFixed(6) + "\n");
+  process.stdout.write((negativeCount / n).toFixed(6) + "\n");
+  process.stdout.write((zeroCount / n).toFixed(6) + "\n");
+}
+
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  plusMinus(arr);
+}
